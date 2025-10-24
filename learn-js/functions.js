8208b73eb21689eb01944db926af07fa9eeff3cf
kumar-jitendra <jitendra.kumar@housing.com>
2025-10-24 21:47:51 +0530

    added git bisect commands

--- learn-js/functions.js
+++ learn-js/functions.js
@@ -14,3 +14,15 @@ function divide(a, b) {
     if (b === 0 ) throw new Error("Division by zero");
     return a / b;
 }
+
+/*
+    Summary of Git Bisect Workflow
+    Step	               Command	                    Description
+    Start bisect	       git bisect start	            Begin binary search
+    Mark bad commit	       git bisect bad	            Usually current HEAD
+    Mark good commit	   git bisect good <commit>	    Old commit without bug
+    Test each commit	    git bisect good/bad	        Repeat until first bad commit is found
+    Finish	               git bisect reset	            Exit bisect mode
+
+After this workflow, you know exactly which commit broke your code. 
+*/
