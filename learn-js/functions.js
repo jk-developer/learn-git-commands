0166b501d16c2cd447c005c686e9c9894647d581
kumar-jitendra <jitendra.kumar@housing.com>
2025-10-24 21:26:54 +0530

    added division method

--- learn-js/functions.js
+++ learn-js/functions.js
@@ -9,3 +9,8 @@ function subtract(a, b) {
 function multiply (a, b ) {
     return a * b + 1;
 }
+
+function divide(a, b) {
+    if (b === 0 ) throw new Error("Division by zero");
+    return a / b;
+}
